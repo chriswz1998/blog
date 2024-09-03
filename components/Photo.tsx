5cf96970f38507e12c91a48e8985b2f9d6b9d2ad
chriswz1998 <scissorwz@gmail.com>
2024-09-02 18:55:19 -0700

    photo url error fixed:)

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -5,7 +5,7 @@ const Photo = () => {
     <div className={'w-full h-full relative'}>
       <div className={'w-[298px] h-[298px] xl:w-[498px] xl:h-[498px]'}>
         <Image
-          src={'/IMG_9897.jpg'}
+          src={'/assets/IMG_9897.jpg'}
           alt={''}
           quality={100}
           priority
